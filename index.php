e8bf8b34db51f91dd8fa52fce9d9963ae80904d5
mariklim <maria.klimenko889@gmail.com>
2021-10-18 16:17:56 +0200

    aggiunto il testo censurato

--- index.php
+++ index.php
@@ -3,20 +3,51 @@ Creare una variabile con un paragrafo di testo a vostra scelta.
 Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+<!-- Paragrafo originale -->
 <?php
-$testo = "Inizia l’autunno e il giardino di Villa Arconati Far si colora di nuovi colori caldi che regalano alla nostra piccola Versailles un fascino ancora più suggestivo, da godere nella luce calda degli ultimi pomeriggi prima del freddo invernale. Da domenica 24 ottobre un orario ridotto, ma sempre le stesse opportunità di godere dell’incanto della villa e del suo giardino.";
+$testo = "La Pizza è uno dei simboli che rappresentano la cucina italiana in tutto il mondo e naturalmente la sua storia è tutta nostrana, ma mentre addentiamo felici una fetta filante di pizza margherita ci è mai venuto in mente di chiederci quando nasce la pizza e chi è stato il primo inventore di questo cibo paradisiaco di cui ormai, concediamocelo, non possiamo più fare a meno? Quale posto è stato testimone della sua nascita e soprattutto chi ne è l’inventore?
+
+Non sono domande a cui è facile dare una risposta perché le origini della pizza sono incerte e un po’ complicate, ma la sua storia millenaria affonda indubbiamente nelle nostre tradizioni.
+Perfetta da gustare in ogni momento della giornata, da gradire con calma in una serata con gli amici e comoda da mangiare se si ha poco tempo a disposizione, così versatile e squisita nella sua semplicità da andare incontro a tutti i gusti, quando e come è nata veramente la prima pizza?";
 ?>
 
-<?php 
-$_GET['parola']
-?>
 
-<h2>Il testo:</h2> 
+<!-- la nuova parola passata tramite Get -->
+<?php $parolaUtente = $_GET['parola']?>
+
+<!-- Visualizazzione: -->
+
+<h2>Il testo:</h2>
+
+<!-- stampo paragrafo originale e la lunghezza -->
 <p>
-    <?php echo $testo?>
+    <?php echo $testo ?>
 </p>
 <h3>La lunghezza del testo:</h3>
 <p>
     <?php echo strlen($testo); ?>
+    caratteri
+</p>
+
+
+<h3>La parola dell'utente:</h3>
+<p>
+    <?php echo $parolaUtente; ?>
+</p>
+
+<!-- Stampo il testo censurato -->
+
+<?php $testoCensurato = str_replace($parolaUtente, "***", $testo)?>
+
+
+<h3>Testo censurato:</h3>
+<p>
+    <?php echo $testoCensurato?>
+</p>
+
+<h3>La lunghezza del testo:</h3>
+<p>
+    <?php echo strlen($testoCensurato); ?>
     caratteri
 </p>
